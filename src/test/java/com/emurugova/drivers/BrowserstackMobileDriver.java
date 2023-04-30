6d65cf5e07d0f2fda1c967116c1fda324330e444
murugka31 <murugka31@mail.ru>
2023-04-30 18:04:01 +0300

    Add main code

--- src/test/java/com/emurugova/drivers/BrowserstackMobileDriver.java
+++ src/test/java/com/emurugova/drivers/BrowserstackMobileDriver.java
@@ -26,7 +26,7 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
 
         desiredCapabilities.setCapability("browserstack.user", config.getBrowserstackUser());
         desiredCapabilities.setCapability("browserstack.key", config.getBrowserstackKey());
-        desiredCapabilities.setCapability("app", "bs://sample.app");
+        desiredCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
         desiredCapabilities.setCapability("device", "Google Pixel 3");
         desiredCapabilities.setCapability("os_version", "9.0");
         desiredCapabilities.setCapability("project", "WikipediaApp");
